ea2048d40ac7a652860c30a792211958d96d7458
peeplilab <akhilesh.pips@gmail.com>
2019-04-09 14:34:21 +0530

    some

---
+++ w3resource/Array/6.js
@@ -0,0 +1,12 @@
+var num = 3232323;
+var str = num.toString();
+var result = [str[0]];
+
+for (var x = 1; x < str.length; x++) {
+  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+    result.push('-', str[x]);
+  } else {
+    result.push(str[x]);
+  }
+}
+console.log(result.join(''));
